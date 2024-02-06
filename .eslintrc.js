ee8700954b0501809a9552b6eef69ac0eee6d041
stroich <stroichmwork@gmail.com>
2024-02-06 19:35:09 +0400

    feat: add mocking tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'prettier/prettier': 0,
   },
 };
